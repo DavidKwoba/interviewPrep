e1f5e174da16e7bd4404dfdfea5fab858eda41c4
David Kwoba <kwoba@stanford.edu>
2022-03-05 13:56:35 -0800

    Second commit

--- Medium/fruitsIntoBasket.py
+++ Medium/fruitsIntoBasket.py
@@ -5,16 +5,14 @@ def totalFruit(fruits):
     fruitTypes, length, prev, maxFruits, types = Counter(), len(fruits), curr, 0, 0
 
     while curr < length:
-        fruit = fruits[curr]
         # Check if it is a new fruit and update the counter
-        if fruitTypes[fruit] == 0: types += 1
-        fruitTypes[fruit] += 1
+        if fruitTypes[fruits[curr]] == 0: types += 1
+        fruitTypes[fruits[curr]] += 1
 
         # We've encountered too many types, start reducing the counter
         while types > 2:
-            prevFruit = fruits[prev]
-            fruitTypes[prevFruit] -= 1
-            if fruitTypes[prevFruit] == 0: types -= 1
+            fruitTypes[fruits[prev]] -= 1
+            if fruitTypes[fruits[prev]] == 0: types -= 1
             prev += 1
 
         # Set maxFruits to the maximum window size
